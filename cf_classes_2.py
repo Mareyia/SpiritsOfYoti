5df236f0df000ec7d67e22693c4147faa8c97181
Mareyia <mareyiazyruk@gmail.com>
2024-12-13 03:01:18 +0200

    A1.4.2.1 experimenting

--- cf_classes_2.py
+++ cf_classes_2.py
@@ -23,20 +23,25 @@ class combinations:
 			move.traverse()
 
 
+
+
+	copy_deck = []
+	for card in decks["Firefly"]:
+		copy_deck.append(card)
+
+	def creat_comb(self, deck):
+		for i in range(len(deck)):
+			copy_deck = []
+			for card in deck:
+				copy_deck.append(card)
+				
+			self.moves.append(copy_deck.pop())
+			for card in copy_deck:
+				self.moves[i].add_move(deck[j])
+
 combination1 = combinations()
-
-copy_deck = []
-for card in decks["Firefly"]:
-	copy_deck.append(card)
-
-for i in range(len(decks["Firefly"])):
-	combination1.moves.append(move(decks["Firefly"][i]))
-	for j in range(len(decks["Firefly"])):
-		if i == j:
-			continue
-		combination1.moves[i].add_move(decks["Firefly"][j])
-
 combination1.traverse()
+
 
 def a_combinations_for_each_card(deck):
 	current_card = self.moves[i]
