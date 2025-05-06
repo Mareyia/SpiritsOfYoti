121b39c8b9e7a3e7bc00fa8f8e1bc7592ff53bc3
Mareyia Zyruk <mareyiazyruk@gmail.com>
2025-05-06 13:55:57 +0300

    No new Version - New Folder System - Fix

--- cf.py
+++
@@ -1,6 +0,0 @@
-from cf_list_dicts import menuuuu
-from cf_maps import maps
-from cf_functions import playGame
-
-print("\n=========================================\n========= Welcome to Card fight =========\n=========================================")
-menuuuu.using_menu(playGame, maps)
